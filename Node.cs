9f0ee368a989280786be72df62545f0f5c6e4c6b
nguyenkhanhtung <tungnk0711@gmail.com>
2020-03-02 17:16:59 +0700

    commit

---
+++ Node.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace linked_list_queue
+{
+    public class Node
+    {
+        public int key;
+        public Node next;
+
+        public Node(int key)
+        {
+            this.key = key;
+            this.next = null;
+        }
+    }
+}
